271c5940d6eaab03f98f6ee86fe5d06b1989f48c
aFleurdelys <Smy99999@qq.com>
2025-04-30 13:26:41 +0800

    fix: 参数

--- app.js
+++ app.js
@@ -3,145 +3,145 @@ import common from "./cfg/common.js";
 
 
 export class App {
-    constructor () {
-    }
+  constructor () {
+  }
 
-    // 社区签到和任务
-    async forumSignIn () {
-        try {
-            // 获取个人信息
-            const mine = await KuroApi.getData('mine');
-            const userId = mine?.data?.mine?.userId;
-            if (!userId) {
-                console.error(`[${ mine.code }]: ${ mine.msg }`);
-                return;
-            }
-            // 2. 获取任务列表
-            const taskList = await KuroApi.getData('taskList', {
-                userId: userId
-            });
-            let dailyTask = taskList?.data?.dailyTask  // 数组
-            if (!dailyTask.length) {
-                console.log('今日无任务，结束。');
-                return;
-            }
-            // 找出 v.needActionTimes - v.completeTimes 的最大值
-            let max = Math.max(...dailyTask.map(v => v.needActionTimes - v.completeTimes));
+  // 社区签到和任务
+  async forumSignIn () {
+    try {
+      // 获取个人信息
+      const mine = await KuroApi.getData('mine');
+      const userId = mine?.data?.mine?.userId;
+      if (!userId) {
+        console.error(`[${ mine.code }]: ${ mine.msg }`);
+        return;
+      }
+      // 2. 获取任务列表
+      const taskList = await KuroApi.getData('taskList', {
+        userId: userId
+      });
+      let dailyTask = taskList?.data?.dailyTask  // 数组
+      if (!dailyTask.length) {
+        console.log('今日无任务，结束。');
+        return;
+      }
+      // 找出 v.needActionTimes - v.completeTimes 的最大值
+      let max = Math.max(...dailyTask.map(v => v.needActionTimes - v.completeTimes));
 
-            // 帖子列表
-            const forumList = await KuroApi.getData('forumList', {
-                forumId: 9,
-                gameId: 3,
-                pageIndex: 1,
-                pageSize: 20,
-            });
-            const postList = forumList?.data?.postList?.slice(0, max) || [] // 数组
-            if (!postList.length) {
-                return;
-            }
-            // 5. 任务映射关系
-            const taskMap = {
-                '社区签到': (post) => KuroApi.getData('forumSignIn', { gameId: 3 }),
-                '浏览3篇帖子': (post) => KuroApi.getData('getPostDetail', { postId: post.postId }),
-                '点赞5次': (post) => KuroApi.getData('like', { postId: post.postId, toUserId: post.userId }),
-                '分享1次帖子': (post) => KuroApi.getData('shareTask', { postId: post.postId }),
-            };
-            // 6. 执行任务
-            console.log('-'.repeat(50))
-            for (const task of dailyTask) {
-                const { remark, process } = task;
-                if (process >= 1.0) {
-                    console.log(`>>>[${ remark }]: ✅已完成`)
-                    continue; // 已完成，跳过
-                }
-                const action = taskMap[remark];
-                for (const post of postList) {
-                    await action(post);
-                    // 每次操作后休眠5秒，防止频繁请求
-                    await common.sleep(5000);
-                }
-                console.log(`>>>[${ remark }]: ✅已完成`);
-            }
-            console.log('-'.repeat(50))
-            // 6. 查询总库洛币
-            const data = await KuroApi.getData('totalGold');
-            if (data?.data) {
-                console.log(`总库洛币: ${ data.data.goldNum } 个`);
-                console.log('-'.repeat(50))
-            }
-        } catch (error) {
-            console.error('执行出错:', error.message);
+      // 帖子列表
+      const forumList = await KuroApi.getData('forumList', {
+        forumId: 9,
+        gameId: 3,
+        pageIndex: 1,
+        pageSize: 20,
+      });
+      const postList = forumList?.data?.postList?.slice(0, max) || [] // 数组
+      if (!postList.length) {
+        return;
+      }
+      // 5. 任务映射关系
+      const taskMap = {
+        '社区签到': (post) => KuroApi.getData('forumSignIn', { gameId: 3 }),
+        '浏览3篇帖子': (post) => KuroApi.getData('getPostDetail', { postId: post.postId }),
+        '点赞5次': (post) => KuroApi.getData('like', { postId: post.postId, toUserId: post.userId }),
+        '分享1次帖子': (post) => KuroApi.getData('shareTask', { postId: post.postId }),
+      };
+      // 6. 执行任务
+      console.log('-'.repeat(50))
+      for (const task of dailyTask) {
+        const { remark, process } = task;
+        if (process >= 1.0) {
+          console.log(`>>>[${ remark }]: ✅已完成`)
+          continue; // 已完成，跳过
         }
-    }
-
-
-    // 游戏签到
-    async gameSignIn (data = {}) {
-        const baseParams = {
-            gameId: data.gameId,
-            serverId: data.serverId,
-            roleId: data.roleId,
-            userId: data.userId
-        };
-        if (data.hasSignIn) {
-            return `❌ ${ data.signInTxt }`
+        const action = taskMap[remark];
+        for (const post of postList) {
+          await action(post);
+          // 每次操作后休眠5秒，防止频繁请求
+          await common.sleep(5000);
         }
-        // 初始化签到
-        await KuroApi.getData('initSignIn', baseParams);
-        // 执行签到
-        const signInParams = {
-            ...baseParams,
-            reqMonth: new Date().toISOString().slice(5, 7) // 自动生成MM格式月份
-        };
-        await KuroApi.getData('signin', signInParams);
-        // 查询签到结果
-        const { data: [ { goodsName, goodsNum } ] } = await KuroApi.getData('queryRecord', baseParams);
-        // 签到物品
-        return `✅ ${ goodsName } * ${ goodsNum }`
+        console.log(`>>>[${ remark }]: ✅已完成`);
+      }
+      console.log('-'.repeat(50))
+      // 6. 查询总库洛币
+      const data = await KuroApi.getData('totalGold');
+      if (data?.data) {
+        console.log(`总库洛币: ${ data.data.goldNum } 个`);
+        console.log('-'.repeat(50))
+      }
+    } catch (error) {
+      console.error('执行出错:', error.message);
     }
+  }
 
 
-    static async run () {
-        let id = Number(process.env.id)
-        let app = new App()
-        try {
-            // 社区
-            if (id === 1) {
-                return await app.forumSignIn()
-            }
-            // 游戏 -- Gray Raven(战双)
-            const isGr = id === 2
-            let params = {
-                type: isGr ? 1 : 2,
-                sizeType: isGr ? 2 : 1,
-                gameId: id
-            }
-            // 获取游戏小组件
-            let result = await KuroApi.getData('widget', params);
-
-            if (result.code !== 200 || !result.data) {
-                console.error(`[${ result.code }]: ${ result.msg }`);
-                return;
-            }
-            let data = result.data;
-            // 签到
-            data.signInTxt = await app.gameSignIn(data)
-            // 数据处理
-            if (isGr) {
-                data.serverName = '战双: 帕弥什'
-                data.energyData = data.actionData
-                data.livenessData = data.dormData
-                data.dailyData = data.activeData
-                data.monthData = await KuroApi.getData('month', { roleId: data.roleId })
-            } else {
-                data.dailyData = data.battlePassData
-            }
-            // 输出日志
-            await common.makeMessage(data)
-        } catch (error) {
-            console.error('执行出错:', error.message);
-        }
+  // 游戏签到
+  async gameSignIn (data = {}) {
+    const baseParams = {
+      gameId: data.gameId,
+      serverId: data.serverId,
+      roleId: data.roleId,
+      userId: data.userId
+    };
+    if (data.hasSignIn) {
+      return `❌ ${ data.signInTxt }`
     }
+    // 初始化签到
+    await KuroApi.getData('initSignIn', baseParams);
+    // 执行签到
+    const signInParams = {
+      ...baseParams,
+      reqMonth: new Date().toISOString().slice(5, 7) // 自动生成MM格式月份
+    };
+    await KuroApi.getData('signin', signInParams);
+    // 查询签到结果
+    const { data: [ { goodsName, goodsNum } ] } = await KuroApi.getData('queryRecord', baseParams);
+    // 签到物品
+    return `✅ ${ goodsName } * ${ goodsNum }`
+  }
+
+
+  static async run () {
+    let id = Number(process.env.id)
+    let app = new App()
+    try {
+      // 社区
+      if (id === 1) {
+        return await app.forumSignIn()
+      }
+      // 游戏 -- Gray Raven(战双)
+      const isGr = id === 2
+      let params = {
+        type: isGr ? 1 : 2,
+        sizeType: isGr ? 2 : 1,
+        gameId: id
+      }
+      // 获取游戏小组件
+      let result = await KuroApi.getData('widget', params);
+
+      if (result.code !== 200 || !result.data) {
+        console.error(`[${ result.code }]: ${ result.msg }`);
+        return;
+      }
+      let data = result.data;
+      // 签到
+      data.signInTxt = await app.gameSignIn(data)
+      // 数据处理
+      if (isGr) {
+        data.serverName = '战双: 帕弥什'
+        data.energyData = data.actionData
+        data.livenessData = data.dormData
+        data.dailyData = data.activeData
+        data.monthData = await KuroApi.getData('month', { roleId: data.roleId })
+      } else {
+        data.dailyData = data.battlePassData
+      }
+      // 输出日志
+      await common.makeMessage(data)
+    } catch (error) {
+      console.error('执行出错:', error.message);
+    }
+  }
 }
 
 
--- cfg/common.js
+++ cfg/common.js
@@ -3,93 +3,93 @@ import _ from 'lodash';
 
 /** 公共函数 */
 const common = {
-    /** 休眠  */
-    async sleep (ms) {
-        return new Promise(resolve => setTimeout(resolve, ms))
-    },
+  /** 休眠  */
+  async sleep (ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+  },
 
-    /** 打印 */
-    async makeMessage (data) {
-        let { gameId, serverName, serverTime, signInTxt, energyData, livenessData, dailyData } = data
+  /** 打印 */
+  async makeMessage (data) {
+    let { gameId, serverName, serverTime, signInTxt, energyData, livenessData, dailyData } = data
 
-        console.log("-".repeat(50));
-        console.log(`游戏：${ serverName }`);
-        console.log(`时间：${ new Date(serverTime * 1000).toLocaleString() }`);
-        console.log(`签到：${ signInTxt }`);
-        console.log("-".repeat(50));
+    console.log("-".repeat(50));
+    console.log(`游戏：${ serverName }`);
+    console.log(`时间：${ new Date(serverTime * 1000).toLocaleString() }`);
+    console.log(`签到：${ signInTxt }`);
+    console.log("-".repeat(50));
 
-        // 收集需要遍历的对象
-        const datas = [ energyData, livenessData, ...[].concat(dailyData) ];  // dailyData可能是数组
+    // 收集需要遍历的对象
+    const datas = [ energyData, livenessData, ...[].concat(dailyData) ];  // dailyData可能是数组
 
-        _.each(datas, (data) => {
-            if (!data) return;  // 防止异常
-            let name = data.name || data.key;
-            if (name === '血清') name = ' 血　清 '; // 美化对齐
+    _.each(datas, (data) => {
+      if (!data) return;  // 防止异常
+      let name = data.name || data.key;
+      if (name === '血清') name = ' 血　清 '; // 美化对齐
 
-            let cur = data.cur ?? 0;
-            let max = data.total || 100; // 有些数据 total 可能缺失，默认100
-            if (gameId === 3) {
-                if (name.includes('结晶波片')) max = 240;
-                if (name.includes('活跃度')) max = 100;
-                if (name.includes('电台等级')) max = 70;
-                if (name.includes('本周经验')) max = 10000;
-            }
-            if (cur > max) cur = max;  // 防止溢出
-            const bar = progBar(cur, max);
+      let cur = data.cur ?? 0;
+      let max = data.total || 100; // 有些数据 total 可能缺失，默认100
+      if (gameId === 3) {
+        if (name.includes('结晶波片')) max = 240;
+        if (name.includes('活跃度')) max = 100;
+        if (name.includes('电台等级')) max = 70;
+        if (name.includes('本周经验')) max = 10000;
+      }
+      if (cur > max) cur = max;  // 防止溢出
+      const bar = progBar(cur, max);
 
-            console.log(`${ name.padEnd(4, '　') }: [${ bar }] ${ cur }/${ max } `);
-        });
-        console.log("-".repeat(50));
-        if (data.bossData) {
-            await bossData(data.bossData)
-            await monthData(data.monthData)
-        }
+      console.log(`${ name.padEnd(4, '　') }: [${ bar }] ${ cur }/${ max } `);
+    });
+    console.log("-".repeat(50));
+    if (data.bossData) {
+      await bossData(data.bossData)
+      await monthData(data.monthData)
     }
+  }
 }
 
 function progBar (cur, max) {
-    const filled = Math.round((cur / max) * 25);
-    return '█'.repeat(filled) + '═'.repeat(25 - filled);
+  const filled = Math.round((cur / max) * 25);
+  return '█'.repeat(filled) + '═'.repeat(25 - filled);
 }
 
 async function bossData (bossData) {
-    bossData.forEach(boss => {
-        let {
-            name, key, value,
-            cur, total,
-            expireTimeStamp, refreshTimeStamp
-        } = boss
-        if (total === 0) total = 100;
-        const times = expireTimeStamp ?? refreshTimeStamp ?? null;
-        const bossBar = progBar(cur, total)
-        console.log(`${ name ?? '' }(${ key ?? '' }) |${ bossBar }| ${ value ?? '' }`.trim());
-        if (times !== null) {
-            console.log(` >>> 刷新时间：${ new Date(times * 1000).toLocaleString() }`);
-        }
-    });
-    console.log("-".repeat(50));
+  bossData.forEach(boss => {
+    let {
+      name, key, value,
+      cur, total,
+      expireTimeStamp, refreshTimeStamp
+    } = boss
+    if (total === 0) total = 100;
+    const times = expireTimeStamp ?? refreshTimeStamp ?? null;
+    const bossBar = progBar(cur, total)
+    console.log(`${ name ?? '' }(${ key ?? '' }) |${ bossBar }| ${ value ?? '' }`.trim());
+    if (times !== null) {
+      console.log(` >>> 刷新时间：${ new Date(times * 1000).toLocaleString() }`);
+    }
+  });
+  console.log("-".repeat(50));
 }
 
 async function monthData (monthData) {
-    let {
-        gameLevel, currentBlackCard,
-        currentDevelopResource, currentTradeCredit,
-        currentMonth, monthBlackCard,
-        monthDevelopResource, monthTradeCredit
-    } = monthData
+  let {
+    gameLevel, currentBlackCard,
+    currentDevelopResource, currentTradeCredit,
+    currentMonth, monthBlackCard,
+    monthDevelopResource, monthTradeCredit
+  } = monthData
 
-    console.log(`游戏等级：${ gameLevel ?? 0 }`);
-    console.log("-".repeat(50));
-    console.log('当前资源：');
-    console.log(` > 黑　　卡 - ${ currentBlackCard ?? 0 }`);
-    console.log(` > 研发资源 - ${ currentDevelopResource ?? 0 }`);
-    console.log(` > 贸易凭据 - ${ currentTradeCredit ?? 0 }`);
-    console.log("-".repeat(50));
-    console.log(`${ currentMonth ?? '' }月资源情况：`);
-    console.log(` > 黑　　卡 - ${ monthBlackCard ?? 0 }`);
-    console.log(` > 研发资源 - ${ monthDevelopResource ?? 0 }`);
-    console.log(` > 贸易凭据 - ${ monthTradeCredit ?? 0 }`);
-    console.log("-".repeat(50));
+  console.log(`游戏等级：${ gameLevel ?? 0 }`);
+  console.log("-".repeat(50));
+  console.log('当前资源：');
+  console.log(` > 黑　　卡 - ${ currentBlackCard ?? 0 }`);
+  console.log(` > 研发资源 - ${ currentDevelopResource ?? 0 }`);
+  console.log(` > 贸易凭据 - ${ currentTradeCredit ?? 0 }`);
+  console.log("-".repeat(50));
+  console.log(`${ currentMonth ?? '' }月资源情况：`);
+  console.log(` > 黑　　卡 - ${ monthBlackCard ?? 0 }`);
+  console.log(` > 研发资源 - ${ monthDevelopResource ?? 0 }`);
+  console.log(` > 贸易凭据 - ${ monthTradeCredit ?? 0 }`);
+  console.log("-".repeat(50));
 }
 
 export default common
